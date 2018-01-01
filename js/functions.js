9174f6598be4e598e765be1ea2946854a24b3a37
tamirandtom <tamirp@gmail.com>
2018-01-01 08:47:16 +0200

    initial

---
+++ js/functions.js
@@ -0,0 +1,14 @@
+$(document).ready(function() { 
+   
+    $(".carrousel").slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        cssEase: 'linear',
+        arrows: false
+    });
+    
+});
