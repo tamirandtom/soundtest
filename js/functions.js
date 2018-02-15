1be33ea75a09f1ae4b9bc703f246d3348ea7abab
tamirandtom <tamirp@gmail.com>
2018-02-15 06:58:32 +0200

    updated carrousel interaction

--- js/functions.js
+++ js/functions.js
@@ -8,8 +8,18 @@ $(document).ready(function() {
         autoplay: true,
         autoplaySpeed: 2000,
         cssEase: 'linear',
-        arrows: false
+        arrows: false,
+        pauseOnHover: false
     });
+
+
+    $('.carrousel').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+        $('.carrousel .slick-dots li').removeClass('slick-active').attr('aria-hidden','true');
+        $('.carrousel .slick-dots li button').focus(function() {
+        this.blur();
+        });
+        });
+
 
     $( ".button" ).click(function() {
     $('html, body').animate({
